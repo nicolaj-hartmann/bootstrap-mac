9d10784cc3e261e7fa211ef6ed7e488c9f2311af
Nicolaj Hartmann <nicolajhartmann@MBPtilheNicolaj.localdomain>
2025-01-16 22:22:05 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-# this is an initial bootstrap of a mac
-
 ensure_line_in_file() {
   local line="$1"
   local file="$2"
@@ -14,7 +12,20 @@ ensure_line_in_file() {
   fi
 }
 
+# Define repository variables
+REPO_URL="git@github.com:nicolaj-hartmann/bootstrap-mac.git"
+REPO_PATH="$HOME/bootstrap-mac"
 
+# Clone repository if it doesn't exist
+if [ ! -d "$REPO_PATH" ]; then
+    echo "Cloning bootstrap repository..."
+    git clone "$REPO_URL" "$REPO_PATH"
+    cd "$REPO_PATH" || exit 1
+else
+    echo "Bootstrap repository already exists"
+    cd "$REPO_PATH" || exit 1
+    git pull
+fi
 
 echo "########################################################"
 echo "#### Checking for Homebrew and adding to PATH if needed"
@@ -45,9 +56,9 @@ else
     echo "Homebrew already in PATH in .zshrc"
 fi
 
-# Add confsync alias to .zshrc
+# Simplified confsync alias
 if ! grep -q "alias confsync=" ~/.zshrc; then
-    echo "alias confsync='(cd ~/bootstrap-mac || git clone git@github.com:nicolaj-hartmann/bootstrap-mac.git ~/bootstrap-mac) && cd ~/bootstrap-mac && git pull && ansible-playbook setup.yml'" >> ~/.zshrc
+    echo "alias confsync='cd $REPO_PATH && git pull && ansible-playbook setup.yml'" >> ~/.zshrc
     echo "Added confsync alias to .zshrc"
 else
     echo "confsync alias already present in .zshrc"
@@ -56,6 +67,20 @@ fi
 # install oh my zsh
 [ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ensure_line_in_file "source $HOMEBREW_PREFIX/share/powerlevel10k/powerlevel10k.zsh-theme" "$HOME/.zshrc" false
+
+P10K_SOURCE="$HOME/.p10k.zsh"
+P10K_TARGET="$REPO_PATH/resources/.p10k.zsh"
+
+if [ -f "$P10K_TARGET" ]; then
+    if [ -f "$P10K_SOURCE" ]; then
+        echo "Removing existing p10k configuration symlink..."
+        rm "$P10K_SOURCE"
+    fi
+    echo "Creating symlink for p10k configuration..."
+    ln -s "$P10K_TARGET" "$P10K_SOURCE"
+else
+    echo "Warning: p10k configuration file not found in home directory $P10K_SOURCE"
+fi
 
 # remove all shortcuts in the dock
 defaults write com.apple.dock persistent-apps -array
