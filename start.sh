ecdd654747f1490b127f8cc2e89340d9db9e9d2f
Nicolaj Hartmann <nhh@softwarehuset.com>
2025-01-16 11:23:39 +0100

    remove zoom from display

--- start.sh
+++ start.sh
@@ -7,6 +7,10 @@ brew install zsh
 
 # remove all shortcuts in the dock
 defaults write com.apple.dock persistent-apps -array
-
 # restart dock
 killall Dock
+
+# remove all zoom from the display
+defaults -currentHost write com.apple.windowserver DisplayResolutionEnabled -bool false
+# restart display
+killall WindowServer
