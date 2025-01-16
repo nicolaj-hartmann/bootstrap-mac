b77d9797af3f337f3a93e2c92a8607f3173faaab
Nicolaj Hartmann <nhh@softwarehuset.com>
2025-01-16 11:12:33 +0100

    initial commit

---
+++ start.sh
@@ -0,0 +1,9 @@
+# this is an initial bootstrap of a mac
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
